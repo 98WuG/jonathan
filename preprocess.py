fe5df6a34bda8c7d9b824683802dbddd2c135ed8
Jonathan Lee <jonalee99@gmail.com>
2019-12-04 02:43:48 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,9 @@
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import pydicom
 import os
 import scipy.ndimage
 import matplotlib.pyplot as plt
 from skimage import measure, morphology
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import re
 import nrrd
 import time
